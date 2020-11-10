196e54d80a6cc668d63f2cfcf901a7ea601cfe11
Myung-gwan Kim <cartopia95@naver.com>
2020-11-10 10:47:37 +0900

    add - flask 엔트리 파일 라우팅

---
+++ app.py
@@ -0,0 +1,31 @@
+import requests
+from flask import Flask, redirect
+from WordCloud import visualize, get_nouns
+
+app = Flask(__name__, static_url_path='', static_folder='static')
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/api/wordCloud/<category>')
+def wordCloud(category):
+    res = requests.get('http://localhost:1323/wordCloud/'+category)
+    words = get_nouns(res.text)
+    visualize(words, category)
+    return redirect('http://localhost:5000/wordCloud_'+category+'.png')
+
+
+@app.route('/api/summary/<petitionID>')
+def summary(petitionID):
+    res = requests.get('http://localhost:1323/summary/'+petitionID)
+    result = res.json()
+    question = result['question']
+    answer = result['answer']
+    return ''
+
+
+if __name__ == '__main__':
+    app.run()
